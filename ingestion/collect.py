def14623c1e0b9aa5e195a588530c721193c6971
John Kwening <kwening.john@gmail.com>
2019-01-23 02:40:19 -0500

    refact: Add citybikes to collect.py
    Add citybikes as a source for data collection workflow.

--- ingestion/collect.py
+++ ingestion/collect.py
@@ -8,82 +8,90 @@ from scrapers.nashbar import NashBar
 from scrapers.performance_bike import PerformanceBikes
 from scrapers.wiggle import Wiggle
 from scrapers.rei import Rei
+from scrapers.citybikes import CityBikes
 
 
 class Collect:
-  """Handles the collection process of source data files."""
-  def __init__(self, mediator, save_data_path=DATA_PATH):
-    self._mediator = mediator
-    self._save_data_path = save_data_path
-    self._SOURCES = [
-      'competitive', 'nashbar', 'performance',
-      'wiggle', 'rei'
-      ]
+    """Handles the collection process of source data files."""
 
-  def _get_class_instance(self, source: str):
-    """Get appropriate scraper class instance for given source."""
-    if source == 'competitive':
-      return CompetitiveCyclist(save_data_path=self._save_data_path)
-    elif source == 'nashbar':
-      return NashBar(save_data_path=self._save_data_path)
-    elif source == 'performance':
-      return PerformanceBikes(save_data_path=self._save_data_path)
-    elif source == 'wiggle':
-      return Wiggle(save_data_path=self._save_data_path)
-    elif source == 'rei':
-      return Rei(save_data_path=self._save_data_path)
-    else:
-      raise ValueError(f'Invalid source: {source} value.')
+    def __init__(self, mediator, save_data_path=DATA_PATH):
+        self._mediator = mediator
+        self._save_data_path = save_data_path
+        self._SOURCES = [
+            'competitive', 'nashbar', 'performance',
+            'wiggle', 'rei', 'citybikes'
+        ]
 
-  def collect_all_products(self, get_specs=True, skip_failed=False):
-    """Collect raw data file from all sources."""
-    for source in self._SOURCES:
-      try:
-        self.collect_products_from_source(source, get_specs=get_specs)
-      except FileNotFoundError as e:
-        if not skip_failed:
-          raise FileNotFoundError(e)
+    def _get_class_instance(self, source: str):
+        """Get appropriate scraper class instance for given source."""
+        if source == 'competitive':
+            return CompetitiveCyclist(save_data_path=self._save_data_path)
+        elif source == 'nashbar':
+            return NashBar(save_data_path=self._save_data_path)
+        elif source == 'performance':
+            return PerformanceBikes(save_data_path=self._save_data_path)
+        elif source == 'wiggle':
+            return Wiggle(save_data_path=self._save_data_path)
+        elif source == 'rei':
+            return Rei(save_data_path=self._save_data_path)
+        elif source == 'citybikes':
+            return CityBikes(save_data_path=self._save_data_path)
         else:
-          print(f'Skipping {source}: {e}')
+            raise ValueError(f'Invalid source: {source} value.')
 
-  def collect_from_sources(self, sources: list, get_specs=True, skip_failed=False):
-    """Collect raw data file from specified sources."""
-    for source in sources:
-      try:
-        self.collect_products_from_source(source, get_specs=get_specs)
-      except FileNotFoundError as e:
-        if not skip_failed:
-          raise FileNotFoundError(e)
-        else:
-          print(f'Skipping {source}: {e}')
+    def collect_all_products(self, get_specs=True, skip_failed=False):
+        """Collect raw data file from all sources."""
+        for source in self._SOURCES:
+            try:
+                self.collect_products_from_source(source, get_specs=get_specs)
+            except FileNotFoundError as e:
+                if not skip_failed:
+                    raise FileNotFoundError(e)
+                else:
+                    print(f'Skipping {source}: {e}')
 
-  def collect_products_from_source(self, source: str, get_specs=True):
-    """Collect raw data file for specified source."""
-    class_ = self._get_class_instance(source)
-    row_datas = class_.get_all_available_prods()
-    print(f'{source} row_datas: {row_datas}')
-    self._mediator.update_manifest(rows=row_datas)
+    def collect_from_sources(self, sources: list, get_specs=True,
+                             skip_failed=False):
+        """Collect raw data file from specified sources."""
+        for source in sources:
+            try:
+                self.collect_products_from_source(source, get_specs=get_specs)
+            except FileNotFoundError as e:
+                if not skip_failed:
+                    raise FileNotFoundError(e)
+                else:
+                    print(f'Skipping {source}: {e}')
 
-    if get_specs:
-      spec_rows = list()
-      for row_data in row_datas:
-        filepath = self._mediator.get_filepath_for_manifest_row(row=row_data)
-        spec_row = class_.get_product_specs(get_prods_from=filepath,
-          bike_type=row_data['bike_type'])
-        spec_rows.append(spec_row)
-      self._mediator.update_manifest(rows=spec_rows)
+    def collect_products_from_source(self, source: str, get_specs=True):
+        """Collect raw data file for specified source."""
+        class_ = self._get_class_instance(source)
+        row_datas = class_.get_all_available_prods()
+        print(f'{source} row_datas: {row_datas}')
+        self._mediator.update_manifest(rows=row_datas)
 
-  def collect_specs_matching(self, source: str, bike_type: str) -> dict:
-    """Collect specs data for given product source and bike_type."""
-    row = self._mediator.get_rows_matching(sources=[source],
-    bike_types=[bike_type], tablenames=['products'])[0]
+        if get_specs:
+            spec_rows = list()
+            for row_data in row_datas:
+                filepath = self._mediator.get_filepath_for_manifest_row(
+                    row=row_data)
+                spec_row = class_.get_product_specs(get_prods_from=filepath,
+                                                    bike_type=row_data[
+                                                        'bike_type'])
+                spec_rows.append(spec_row)
+            self._mediator.update_manifest(rows=spec_rows)
 
-    filepath = self._mediator.get_filepath_for_manifest_row(row)
+    def collect_specs_matching(self, source: str, bike_type: str) -> dict:
+        """Collect specs data for given product source and bike_type."""
+        row = self._mediator.get_rows_matching(sources=[source],
+                                               bike_types=[bike_type],
+                                               tablenames=['products'])[0]
 
-    class_ = self._get_class_instance(source)
-    spec_row_data = class_.get_product_specs(get_prods_from=filepath,
-      bike_type=bike_type)
-    print(f'[collect_specs_matching] {source} spec_row_data: {spec_row_data}')
-    self._mediator.update_manifest(rows=[spec_row_data])
-    return spec_row_data
-    
+        filepath = self._mediator.get_filepath_for_manifest_row(row)
+
+        class_ = self._get_class_instance(source)
+        spec_row_data = class_.get_product_specs(get_prods_from=filepath,
+                                                 bike_type=bike_type)
+        print(
+            f'[collect_specs_matching] {source} spec_row_data: {spec_row_data}')
+        self._mediator.update_manifest(rows=[spec_row_data])
+        return spec_row_data
