93848cc7ad70ac469959eff239795c8dba65bebe
John Kwening <kwening.john@gmail.com>
2019-01-26 22:41:48 -0500

    feat: Add bicyclesproshop.com scraper
    Added logic for scraping bicyclesproshop.com, including unit tests.

--- tests/proshop_test.py
+++ tests/proshop_test.py
@@ -7,7 +7,7 @@ import json
 from bs4 import BeautifulSoup
 
 # package modules
-from scrapers.citybikes import CityBikes
+from scrapers.proshop import Proshop
 
 #######################################
 #  MODULE CONSTANTS
@@ -17,90 +17,93 @@ MODULE_DIR = os.path.abspath(os.path.dirname(__file__))
 DATA_PATH = os.path.abspath(os.path.join(MODULE_DIR, 'data'))
 TEST_DATA_PATH = os.path.abspath(os.path.join(MODULE_DIR, 'test_data'))
 HTML_PATH = os.path.abspath(os.path.join(MODULE_DIR, 'test_html'))
-TEST_PROD_LISTING_PATH = os.path.join(TEST_DATA_PATH,
-                                      'performance_prod_listing_test_data.csv')
 SHOP_BIKES_HTML_PATH = os.path.abspath(
-    os.path.join(HTML_PATH, 'citybikes.html'))
-COMMUTER_BIKES_HTML_PATH = os.path.abspath(
-    os.path.join(HTML_PATH, 'citybikes-Commuter_Urban.html')
+    os.path.join(HTML_PATH, 'proshop.html'))
+MOUNTAIN_BIKES_HTML_PATH = os.path.abspath(
+    os.path.join(HTML_PATH, 'proshop-mountain.html')
 )
-DRAGONFLY_SPECS = {
-    'frame': 'Reynolds 853 chromoly, w/dropper post routing, ISCG05, sliding 12x142mm dropouts',
-    'fork': 'Fox 32 Float Performance Series, 120mm-travel w/tapered steerer, 15mm thru-axle',
-    'rims_wheels': 'WTB Frequency Team i23 TCS',
-    'hubs': 'Formula disc',
-    'spokes': 'Stainless-steel',
-    'tires': 'Vittoria Barzo, 27.5 x 2.25',
-    'crankset': 'Shimano',
-    'chainrings': '36/22',
-    'front_derailleur': 'Shimano Deore',
-    'rear_derailleur': 'Shimano SLX Shadow',
-    'rear_cogs': 'Shimano, 10-speed: 11-36',
-    'shifters': 'Shimano SLX',
-    'handlebars': 'Ritchey Trail',
-    'tape_grips': 'Jamis lock-on',
-    'stem': 'Ritchey Trail',
-    'brakes': 'Shimano Deore disc, 180/160mm rotors',
-    'saddle': 'WTB Volt Comp w/Luxe Zone Cut-Out',
-    'seatpost': 'Ritchey Trail'}
-CROSS_TRAIL_SPECS = {
-    'frame': 'Specialized A1 Premium Aluminum, Fitness Geometry, butted tubing, rack mounts, Plug + Play fender mounts',
-    'fork': 'SR Suntour NEX w/ Specialized Fitness Brain technology, 55mm of travel, 1-1/8" steerer, QR, fender mounts',
-    'rims_wheels': '700C disc, double wall',
-    'tires': 'Trigger Sport Reflect, 60 TPI, wire bead, 700x38mm',
-    'crankset': 'Shimano Tourney, 3-piece',
-    'chainrings': '48/38/28T',
-    'bottom_bracket': 'BSA, 68mm, square taper',
-    'chain': 'KMC X8EPT, 8-speed, anti-corrosion coating w/ reusable Missing Link',
-    'front_derailleur': 'Shimano Tourney, top swing, 31.8mm clamp',
-    'rear_derailleur': 'Shimano Altus, 8-Speed',
-    'cassette_rear_cogs': 'Sunrace, 8-speed, 11-34t',
-    'shifters': 'Shimano Altus, RapidFire Plus, w/ gear display',
-    'handlebars': 'Double-butted alloy, 9-degree backsweep, 31.8mm',
-    'tape_grips': 'Specialized Body Geometry XCT, lock-on',
-    'stem': '3D forged alloy, 7-degree rise, 31.8mm clamp',
-    'brakes': 'Promax Solve, hydraulic disc, post mount, 160mm rotor',
-    'saddle': 'Specialized Canopy Comp, hollow Cr-Mo rails, 155mm',
-    'seat_post': 'Alloy, 12mm offset, 2-bolt clamp, 27.2mm'}
+SIRRUS_SPECS = {
+    'frame': 'Specialized A1 Premium aluminum',
+    'fork': 'Specialized steel',
+    'rims_wheels': 'Aluminum, double-wall',
+    'hubs': 'Aluminum, sealed',
+    'spokes': '14-gauge, stainless-steel',
+    'tires': 'Specialized Nimbus, 700 x 32c w/Flak Jacket puncture protection',
+    'crankset': 'Specialized Stout',
+    'chainrings': '48/38/28',
+    'front_derailleur': 'Shimano',
+    'rear_derailleur': 'Shimano Altus',
+    'rear_cogs': 'Shimano, 8-speed: 11-32',
+    'shifters': 'Shimano',
+    'handlebars': 'Specialized flat, aluminum',
+    'tape_grips': 'Specialized Body Geometry Targa',
+    'stem': 'Aluminum',
+    'brakes': 'V-Brake',
+    'pedals': 'Platform',
+    'saddle': 'Specialized Canopy Sport',
+    'seatpost': 'Aluminum'}
+SURLY_SPECS = {
+    'frame': 'Surly 4130 chromoly steel; custom double-butted, externally flared tubing; horizontal slotted with derailleur hanger; Surly “Gnot Boost” 142 or 148mm hub spacing compatibility',
+    'fork': '4130 chromoly, 483mm axle-to-crown x 47mm offset, 110mm hub spacing, tapered and butted straight blade, 51mm I.S. disc mount',
+    'headset': 'Cane Creek',
+    'axles': 'Front: 110 x 15mm TA',
+    'rims_wheels': 'Alex MD40',
+    'hubs': 'Salsa 32h',
+    'tires': 'Surly Dirt Wizard 29 x 3, 60 tpi',
+    'crankset': 'SRAM NX',
+    'chainrings': '30T',
+    'bottom_bracket': 'SRAM',
+    'chain': 'KMX X11-1',
+    'rear_derailleur': 'SRAM NX',
+    'cassette_rear_cogs': 'Sun Race, 11-speed: 11-42',
+    'shifters': 'SRAM NX',
+    'handlebars': 'Answer Pro Taper, 31.8mm',
+    'stem': 'H.L. 4-bolt',
+    'brake_levers': 'SRAM Level',
+    'brakes': 'SRAM Level hydraulic disc, 180mm rotors',
+    'saddle': 'WTB Volt Sport',
+    'seat_post': 'H.L. 30.9mm'}
 
 
-class CityBikesTestCase(unittest.TestCase):
+class ProshopTestCase(unittest.TestCase):
     def setUp(self):
-        self._citybikes = CityBikes(save_data_path=DATA_PATH)
+        self._proshop = Proshop(save_data_path=DATA_PATH)
 
     def test_fetch_prod_listing_view(self):
-        text = self._citybikes._fetch_prod_listing_view(
-            endpoint=self._citybikes._PROD_PAGE_ENDPOINT, page_size=30)
+        text = self._proshop._fetch_prod_listing_view(
+            endpoint=self._proshop._PROD_PAGE_ENDPOINT, page_size=30)
         soup = BeautifulSoup(text, 'lxml')
-        self._citybikes._get_prods_on_current_listings_page(
+        self._proshop._get_prods_on_current_listings_page(
             soup, bike_type='all')
-        self.assertEqual(30, len(self._citybikes._products),
+        self.assertEqual(30, len(self._proshop._products),
                          msg='First page should return 30 products.')
 
     def test_get_categories(self):
         categories = {
-            'road': {'filter_par': 'rb_ct', 'filter_val': 1001, 'count': 24},
+            'road': {'filter_par': 'rb_ct', 'filter_val': 1001, 'count': 256},
             'mountain': {'filter_par': 'rb_ct', 'filter_val': 1006, 'count':
-                19},
+                446},
             'commuter_urban': {'filter_par': 'rb_ct', 'filter_val': 1017,
-                               'count': 65},
+                               'count': 143},
+            'cyclocross': {'filter_par': 'rb_ct', 'filter_val': 1014,
+                               'count': 49},
             'comfort': {'filter_par': 'rb_ct', 'filter_val': 1020, 'count':
-                13},
-            'cruiser': {'filter_par': 'rb_ct', 'filter_val': 1021, 'count':
-                1},
-            'fitness': {'filter_par': 'rb_ct', 'filter_val': 1249, 'count':
-                32},
+                26},
+            'fitness': {'filter_par': 'rb_ct', 'filter_val': 1902, 'count':
+                90},
             'hybrid': {'filter_par': 'rb_ct', 'filter_val': 1022, 'count':
-                40},
+                113},
             'childrens': {'filter_par': 'rb_ct', 'filter_val': 1023, 'count':
-                20},
+                48},
             'other': {'filter_par': 'rb_ct', 'filter_val': 1037, 'count':
-                28}
+                34},
+            'bmx': {'filter_par': 'rb_ct', 'filter_val': 1032,
+                               'count': 10}
         }
 
         with open(SHOP_BIKES_HTML_PATH, mode='r', encoding='utf-8') as html:
             soup = BeautifulSoup(html, 'lxml')
-        result = self._citybikes._get_categories(soup)
+        result = self._proshop._get_categories(soup)
         for title in categories.keys():
             cat = categories[title]
             r_cat = result[title]
@@ -111,21 +114,21 @@ class CityBikesTestCase(unittest.TestCase):
                                     f'{cat[key]}')
 
     def test_get_prod_listings(self):
-        with open(COMMUTER_BIKES_HTML_PATH, mode='r',
+        with open(MOUNTAIN_BIKES_HTML_PATH, mode='r',
                   encoding='utf-8') as html:
             soup = BeautifulSoup(html, 'lxml')
-        self._citybikes._get_prods_on_current_listings_page(
-            soup, 'commuter_urban')
-        self.assertEqual(30, len(self._citybikes._products),
-                         msg='First page should return 60 products.')
+        self._proshop._get_prods_on_current_listings_page(
+            soup, 'mountain')
+        self.assertEqual(30, len(self._proshop._products),
+                         msg='First page should return 3pro0 products.')
 
     def test_get_all_available_prods(self):
-        result = self._citybikes.get_all_available_prods()
+        result = self._proshop.get_all_available_prods()
 
         total_bikes = 0
-        for values in self._citybikes._BIKE_CATEGORIES.values():
+        for values in self._proshop._BIKE_CATEGORIES.values():
             total_bikes += values['count']
-        num_prods = len(self._citybikes._products)
+        num_prods = len(self._proshop._products)
         # There are dupes so expect less num_prods
         self.assertTrue(total_bikes >= num_prods,
                         msg=f'expected: {total_bikes} - found: {num_prods}')
@@ -133,41 +136,41 @@ class CityBikesTestCase(unittest.TestCase):
     def test_parse_prod_spec(self):
         # load test prod details into memory
         html_path = os.path.abspath(os.path.join(
-            HTML_PATH, 'citybikes-Specialized-CrossTrail-Hydraulic-Disc.html'))
+            HTML_PATH, 'proshop-surly.html'))
         with open(html_path, encoding='utf-8') as f:
-            cross_trail_prod_detail_text = f.read()
+            surly_prod_detail_text = f.read()
 
         html_path = os.path.abspath(os.path.join(
-            HTML_PATH, 'citybikes-Jamis-Dragonfly-Women.html'))
+            HTML_PATH, 'proshop-specialized-sirrus.html'))
         with open(html_path, encoding='utf-8') as f:
-            dragonfly_prod_detail_text = f.read()
+            sirrus_prod_detail_text = f.read()
 
         html_path = os.path.abspath(os.path.join(
-            HTML_PATH, 'citybikes-Specialized-Boys-Hotwalk.html'))
+            HTML_PATH, 'proshop-strider.html'))
         with open(html_path, encoding='utf-8') as f:
             generic_error = f.read()
 
-        cross_trail_detail_soup = BeautifulSoup(
-            cross_trail_prod_detail_text, 'lxml')
-        dragonfly_detail_soup = BeautifulSoup(
-            dragonfly_prod_detail_text, 'lxml')
+        surly_detail_soup = BeautifulSoup(
+            surly_prod_detail_text, 'lxml')
+        sirrus_detail_soup = BeautifulSoup(
+            sirrus_prod_detail_text, 'lxml')
         generic_error_soup = BeautifulSoup(generic_error, 'lxml')
 
         # case 1: exact match per example data
-        result = self._citybikes._parse_prod_specs(cross_trail_detail_soup)
-        self.assertEqual(len(CROSS_TRAIL_SPECS), len(result))
-        for key in CROSS_TRAIL_SPECS.keys():
+        result = self._proshop._parse_prod_specs(surly_detail_soup)
+        self.assertEqual(len(SURLY_SPECS), len(result))
+        for key in SURLY_SPECS.keys():
             self.assertEqual(
-                CROSS_TRAIL_SPECS[key], result[key])
+                SURLY_SPECS[key], result[key])
 
         # case 2: using second data, exact match in components
-        result = self._citybikes._parse_prod_specs(dragonfly_detail_soup)
-        self.assertEqual(len(DRAGONFLY_SPECS), len(result))
-        for key in DRAGONFLY_SPECS.keys():
-            self.assertEqual(DRAGONFLY_SPECS[key], result[key])
+        result = self._proshop._parse_prod_specs(sirrus_detail_soup)
+        self.assertEqual(len(SIRRUS_SPECS), len(result))
+        for key in SIRRUS_SPECS.keys():
+            self.assertEqual(SIRRUS_SPECS[key], result[key])
 
         # case 3: safely handle missing specs
-        result = self._citybikes._parse_prod_specs(generic_error_soup)
+        result = self._proshop._parse_prod_specs(generic_error_soup)
         self.assertEqual(0, len(result))
 
 
