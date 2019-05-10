9d9fb7ad45d736a4ad48e141e114090dbf58f37a
John Kwening <kwening.john@gmail.com>
2019-05-09 22:15:50 -0400

    refact: Bicycle warehouse prod specs
    Simplify parsing logic to allow parsing products specs irrelevant of inconsistent website html structure.

--- tests/bicycle_warehouse_test.py
+++ tests/bicycle_warehouse_test.py
@@ -24,9 +24,10 @@ SHOP_ROAD_FROM_HTML_PATH = os.path.abspath(os.path.join(
     HTML_PATH, 'bicycle-warehouse-roadbikes-from.html'))
 SHOP_NO_NEXT_HTML_PATH = os.path.abspath(os.path.join(
     HTML_PATH, 'bicycle-warehouse-no-next.html'))
-SAMPLE_SPECS1 = {'frame': 'Aluminum 6061, tapered head tube, rack, lock, and fender bosses', 'fork': 'Rigid alloy, tapered, thru-axle', 'headset': 'FSA semi-integrated ahead', 'cranks': '42T w/Double Alloy Guard', 'rear_derailleur': 'Shimano SLX', 'shifter': 'Shimano SLX', 'cogset_causette_freewheel': 'Shimano 11-32T', 'chain': 'KMC X10eRB high torque rust proof', 'front_hub': 'Alloy 15mm thru axle', 'rear_hub': 'Alloy 142/12mm rear', 'spokes': 'Stainless', 'rims': 'Alex DH19 doublewall', 'tires': 'Kenda Kwick Bitumen 700x45c', 'brakes': 'Shimano M396 hydraulic disc, 180mm rotors', 'brake_levers': 'Shimano M396 hydraulic', 'pedals': 'Alloy platform', 'handlebar': 'Alloy 31.8, 640mmx15mm', 'grips': 'Velo dual density', 'stem': 'TranzX 3D forged alloy, 31.8mm', 'seat': 'Velo street', 'seatpost': 'TranzX alloy 31.6x350mm', 'motor': 'Currie Electro-Drive® Centerdrive 500W', 'battery': 'Lithium-ion 48V8.7Ah 417Wh', 'display': 'COBI – The first smart connected biking system', 'max_assisted_speed': '28 mph (45 kph)', 'range_estimated': '16 mi (26 km) to 35 mi (56 km). Varies based on terrain and rider input.', 'accessories': 'Kickstand, fenders, rack, lightset, lock'}
+SAMPLE_SPECS1 = {'frame': 'Reynolds 631 Chromoly Custom Butted, Tapered Headtube, 142x12mm Thru Axles, Disc Tabs', 'fork': 'Carbon/Alloy Tapered Steer, Post Mount Disc, 15mm Thru Axles', 'headset': 'Integrated Cartridge Bearings', 'cranks': 'Praxis Works Alba M30, 38 Tooth Direct Mount', 'bottom_bracket': 'Praxis Works M30 External Bearings', 'rear_derailleur': 'SRAM Rival 1, 11 Speed', 'shifter': 'SRAM Rival 1 HRD, 11 Speed', 'cogset_cassette_freewheel': 'SRAM PG1130, 11 Speed, 11-42', 'chain': 'KMC X11EL-1', 'front_hub': 'Alloy Disc, Cartridge Bearing, 15mm Thru Axle, 28 Hole', 'rear_hub': 'Alloy Disc, Cartridge Bearing, Thru Axle, 28 Hole', 'spokes': '14/15g Butted Stainless with Brass Nipples', 'rims': 'HED Tomcat Disc, 28 Hole, Tubless Compatible', 'tires': "Clement X'PLOR MSO, 700x40c, 60TPI, Folding", 'brakes': 'SRAM Rival Hydraulic Disc, 160mm Rotors', 'brake_levers': 'SRAM Rival', 'pedals': 'Resin Platform Pedal', 'handlebar': 'HED Eroica, 31.8 with 16 Degree Flare, 38/40/42/44/46', 'stem': 'HED Eroica, 31.8, Lengths:/90/100/110/mm', 'seat': 'WTB Volt Race', 'seatpost': 'HED Eroica, 27.2', 'extras': 'Tubeless Valves, Rack and Fender Mounts, third bottle cage mount, Travel Bag'}
 SAMPLE_SPECS2 = {'sizes': 'XS, S, M, M/L, L, XL', 'colors': 'Neon Red / Black Chrome, Matte Black / Gloss Black', 'frame': 'ALUXX SL-Grade Aluminum', 'fork': 'Advanced-Grade Composite, alloy OverDrive steerer', 'shock': 'N/A', 'handlebar': 'Giant Connect flat', 'stem': 'Giant Connect, 8-degree', 'seatpost': 'Giant D-Fuse, composite', 'saddle': 'Giant Contact (neutral)', 'pedals': 'Platform', 'shifters': 'Shimano Tiagra, 2x10', 'front_derailleur': 'Shimano Tiagra', 'rear_derailleur': 'Shimano Tiagra', 'brakes': 'TRP flat mount HD-R210 {F] 140mm rotor [R] 140mm rotor, custom caliper', 'brake_levers': 'TRP HD-R210', 'cassette': 'CS-HG500, 11x34', 'chain': 'KMC X10 with Missing Link', 'crankset': 'FC-RS400, 34/50', 'bottom_bracket': 'Shimano, threaded', 'rims': 'Giant S-R2 disc, tubeless wheelset', 'hubs': 'Giant S-R2 disc, tubeless wheelset', 'spokes': 'Giant S-R2 disc, tubeless wheelset', 'tires': 'Gavia AC 2 tubeless, 700x28', 'extras': 'Contact Ergo Max bar end'}
 SAMPLE_SPECS3 = {'sizes': 'XS, S, M, L, XL', 'colors': 'Metallic Black / Metallic Orange, Glacier Green/ Pure Red / Navy Blue', 'frame': 'ALUXX SL-Grade Aluminum', 'fork': 'Suntour AION 35-Boost RC DS 27.5+, 150mm travel, Boost QR15x110mm, tapered steerer', 'shock': 'RockShox Deluxe R, trunnion mount', 'handlebar': 'Giant Connect Trail, 780x31.8mm', 'stem': 'Giant Connect', 'seatpost': 'Giant Contact Switch dropper post with remote lever, 30.9mm', 'saddle': 'Giant Contact (neutral)', 'pedals': 'N/A', 'shifters': 'Shimano Deore, 1x10', 'front_derailleur': 'N/A', 'rear_derailleur': 'Shimano Deore , Shadow+', 'brakes': 'Shimano BR-MT400 [F] 180mm [R] 180mm, hydraulic disc', 'brake_levers': 'Shimano BL-MT400', 'cassette': 'Shimano Deore CS-HG500-10, 11x42', 'chain': 'KMC X10-1', 'crankset': 'Praxis Cadet, Boost, 30', 'bottom_bracket': 'Praxis BB-90 Press Fit', 'rims': 'Giant AM 27.5, tubeless ready, sleeve-joint rim, 30mm inner width', 'hubs': '[F] Giant Tracker Performance Boost 15x110mm, sealed bearing [R] Giant Tracker Performance, Boost 12x148, sealed bearing', 'spokes': 'Sapim', 'tires': '[F] Maxxis high Roller II 27.5x2.5 WT, 60 tpi, EXO, TR [R] Maxxis high Roller II 27.5x2.4, 60 tpi, EXO, TR, tubeless'}
+SAMPLE_SPECS4 = {'available_frame_sizes': 'S, M, L', 'available_colors': 'SANTA FE SAND; MILLITARY GREEN / CEMENT GREY', 'frame': 'NINER RDO CARBON FIBER, 140MM TRAVEL, GEO FLIP CHIP, RIB CAGE CONSTRUCTION, FULL SLEEVE CABLE ROUTING, ENDURO MAX BLACK OXIDE PIVOT BEARINGS', 'fork': 'FOX 36 FLOAT RHYTHM GRIP EVOL, SWEEP ADJUST, 150MM, 110X15MM, 44MM OFFSET', 'shock': 'FOX FLOAT DPX2 PERFORMANCE EVOL 3 POSITION', 'tubes_sealant': 'STANS NO TUBES SEALANT (2 X 2OZ BOTTLES)', 'front_wheel': 'NINER ALLOY, 110X15MM FRONT, NINER GRAPHIC', 'rear_wheel': 'NINER ALLOY, 148X12MM REAR, NINER GRAPHIC', 'front_tire': 'MAXXIS MINION DHF 3C/EXO/TR 2.5 WT FRONT', 'rear_tire': 'MAXXIS AGGRESSOR 2C/EXO/TR 2.5 WT REAR', 'brakes': 'SRAM LEVEL', 'brake_levers': 'SRAM LEVEL', 'brake_rotors': '180/180MM G2CS ROTORS', 'chain': 'SRAM NX EAGLE 12SP', 'front_shifter': 'N/A', 'rear_shifter': 'SRAM NX EAGLE 12SP', 'front_derailleur': 'N/A', 'rear_derailleur': 'SRAM PG 1230 11-50T', 'cassette': 'SRAM NX EAGLE 12SP', 'crankset': 'SRAM NX EAGLE DUB 32T', 'bottom_bracket': 'SRAM DUB BSA THREADED', 'saddle': 'NINER CUSTOM TR WITH CR-MO RAILS, PRINTED NINER GRAPHIC', 'seatpost': 'SDG TELLIS (S-125MM, M-150MM, L/XL-170MM)', 'handlebar': 'RACE FACE AEFFECT R 780MM WIDE, 20MM RISE, 35MM CLAMP', 'stem': 'RACE FACE AEFFECT R 40MM, 35MM CLAMP', 'headset': 'NINER INTERNAL ZS SHIS DESCRIPTION ZS44/28.6|ZS56/40', 'grips': 'NINER GRRRIPS L/O NYLON FLANGED'}
 
 
 class SpecializedTestCase(unittest.TestCase):
@@ -126,12 +127,20 @@ class SpecializedTestCase(unittest.TestCase):
         with open(html_path, encoding='utf-8') as f:
             prod_detail_text3 = f.read()
 
+        html_path = os.path.abspath(os.path.join(
+            HTML_PATH, 'bicycle-warehouse-4cols.html'))
+        with open(html_path, encoding='utf-8') as f:
+            prod_detail_text4 = f.read()
+
         detail_soup1 = BeautifulSoup(
             prod_detail_text1, 'lxml')
         detail_soup2 = BeautifulSoup(
             prod_detail_text2, 'lxml')
         detail_soup3 = BeautifulSoup(
             prod_detail_text3, 'lxml'
+        )
+        detail_soup4 = BeautifulSoup(
+            prod_detail_text4, 'lxml'
         )
 
         # case 1: exact match per example data
@@ -154,6 +163,12 @@ class SpecializedTestCase(unittest.TestCase):
             self.assertEqual(SAMPLE_SPECS3[key], result[key],
                              msg=f'{key}: Expected - {SAMPLE_SPECS3[key]}; '
                              f'Result - {result[key]}')
+
+        # case 4: 4 columns in specs table
+        result = self._scraper._parse_prod_specs(detail_soup4)
+        self.assertEqual(len(SAMPLE_SPECS4), len(result))
+        for key in SAMPLE_SPECS4.keys():
+            self.assertEqual(SAMPLE_SPECS4[key], result[key])
 
 
 if __name__ == '__main__':
