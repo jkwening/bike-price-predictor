7d65c5829595d24e35f85a2173e92d58b93877c5
John Kwening <kwening.john@gmail.com>
2020-01-11 09:28:35 -0500

    refact: Rei scraper
    Update scraper to include bike subtypes and add details/description when parsing tech specifications.

--- scrapers/rei.py
+++ scrapers/rei.py
@@ -3,6 +3,7 @@ Module for scraping rei.com for its bike data.
 """
 import json
 import math
+import functools
 
 from bs4 import BeautifulSoup
 
@@ -14,27 +15,25 @@ class Rei(Scraper):
         self._page_size = page_size
         super().__init__(base_url='http://www.rei.com',
                          source='rei', save_data_path=save_data_path)
-        self._BIKES_ENDPOINT = 'c/bikes'
+        self._BIKES_ENDPOINT = '/c/bikes'
 
     def _fetch_prod_listing_view(self, endpoint=None, bike='bikes',
                                  page=1, page_size=90):
         if endpoint is None:
-            endpoint = 'search-ui/rest/search/products/results?'
+            endpoint = '/search-ui/rest/search/products/results?'
             r = f'category%3A{bike}'
             origin = 'web'
-            req_url = f'{self._BASE_URL}/{endpoint}r={r}&origin={origin}&page={page}&pagesize={page_size}'
+            req_url = f'{self._BASE_URL}{endpoint}r={r}&origin={origin}&page={page}&pagesize={page_size}'
         else:
-            req_url = f'{self._BASE_URL}/{endpoint}'
+            req_url = f'{self._BASE_URL}{endpoint}'
 
         return self._fetch_html(req_url)
 
-    def _get_categories(self):
-        """Return bike category links from general bike page."""
+    def _parse_categories(self, soup, exclude) -> dict:
         categories = dict()
-        html = self._fetch_prod_listing_view(endpoint=self._BIKES_ENDPOINT)
-        soup = BeautifulSoup(html, 'lxml')
-
         div_categories = soup.find(id='filter-Categories')
+        if div_categories is None:
+            return dict()
         a_tags = div_categories.find_all('a')
 
         for a in a_tags:
@@ -44,11 +43,34 @@ class Rei(Scraper):
             bike_type, count = bike_type.split('(')
             bike_type = bike_type.replace('bikes', '').strip()
             bike_type = self._normalize_spec_fieldnames(bike_type)
+            if bike_type in exclude:
+                continue
             count = int(count.strip(')').strip())
             cat['total'] = count
             categories[bike_type] = cat
-
         return categories
+
+    def _get_categories(self):
+        """Return bike category links from general bike page."""
+        exclude = ['kids', 'bike_simulators']
+        html = self._fetch_prod_listing_view(endpoint=self._BIKES_ENDPOINT)
+        soup = BeautifulSoup(html, 'lxml')
+        return self._parse_categories(soup, exclude)
+
+    def _get_subtypes(self) -> dict:
+        exclude = []
+        subtypes = dict()
+        categories = self._get_categories()
+        for bike_type in categories.keys():
+            href = categories[bike_type]['href']
+            html = self._fetch_prod_listing_view(endpoint=href)
+            soup = BeautifulSoup(html, 'lxml')
+            result = self._parse_categories(soup, exclude)
+            if result:
+                subtypes[bike_type] = result
+            else:
+                subtypes[bike_type] = categories[bike_type]
+        return subtypes
 
     # TODO: seems unnecessary, remove and embed directly into get_all_available_prods()
     def _get_max_num_prods(self, soup):
@@ -63,40 +85,47 @@ class Rei(Scraper):
         self._num_bikes = 0
 
         # Get products for each bike type category
-        categories = self._get_categories()
-        for bike_type in categories.keys():
-            bike = categories[bike_type]['href'].split('/')[-1]
-            page_num = 1
-            data = json.loads(
-                self._fetch_prod_listing_view(
-                    page_size=self._page_size, bike=bike
-                )
-            )
-
-            num_pages = math.ceil(int(data['query']['totalResults']) / int(data['query']['upperResult']))
-
-            # Scrape first page while its in memory then fetch and scrape the remaining pages
-            self._get_prods_on_current_listings_page(data, bike_type)
-            while True:
-                page_num += 1
-                if page_num > num_pages:
-                    break
-                else:
-                    data = json.loads(self._fetch_prod_listing_view(
-                        page=page_num, page_size=self._page_size, bike=bike)
+        categories = self._get_subtypes()
+        for bike_type, subtypes in categories.items():
+            for subtype, values in subtypes.items():
+                # only need subtype url name not entire endpoint
+                bike = values['href'].split('/')[-1]
+                page_num = 1
+                data = json.loads(
+                    self._fetch_prod_listing_view(
+                        page_size=self._page_size, bike=bike
                     )
-                    self._get_prods_on_current_listings_page(data, bike_type)
+                )
+
+                num_pages = math.ceil(int(data['query']['totalResults']) / int(data['query']['upperResult']))
+
+                # Scrape first page while its in memory then fetch and scrape the remaining pages
+                self._get_prods_on_current_listings_page(
+                    data, bike_type, subtype
+                )
+                while True:
+                    page_num += 1
+                    if page_num > num_pages:
+                        break
+                    else:
+                        data = json.loads(self._fetch_prod_listing_view(
+                            page=page_num, page_size=self._page_size, bike=bike)
+                        )
+                        self._get_prods_on_current_listings_page(
+                            data, bike_type, subtype
+                        )
 
         if to_csv:
             return [self._write_prod_listings_to_csv()]
 
         return list()
 
-    def _get_prods_on_current_listings_page(self, data, bike_type):
+    def _get_prods_on_current_listings_page(self, data, bike_type, subtype):
         results = data['results']
 
         for prod in results:
-            product = {'site': 'rei', 'bike_type': bike_type}
+            product = {'site': 'rei', 'bike_type': bike_type,
+                       'subtype': subtype}
             brand = prod['brand']
             title = prod['cleanTitle']
             product['brand'] = brand
@@ -114,18 +143,28 @@ class Rei(Scraper):
         """Return dictionary representation of the product's specification."""
         prod_specs = dict()
 
-        # Get correct scrip json per rei or rei-garage
+        # Get correct json data per rei or rei-garage/outlet
         if garage:
             script = soup.find('script',
                                attrs={'id': 'page-data'})
             data = json.loads(script.string)
             specs = data['product']['specifications']['specs']
+            prod_details = data['product']['features']
         else:
             script = soup.find('script',
                                attrs={'data-client-store': 'product-details'})
             data = json.loads(script.string)
             specs = data['specs']
+            prod_details = data['features']
 
+        # parse details/description
+        details = ''
+        for detail in prod_details:
+            details += detail + '\n'
+        details = details.strip()
+        prod_specs['details'] = details
+
+        # parse tech specifications
         try:
             # Get each spec_name, value pairing for bike product
             for spec in specs:
--- tests/scrapers/rei_test.py
+++ tests/scrapers/rei_test.py
@@ -11,50 +11,62 @@ from utils.unit_test_utils import DATA_PATH, TIMESTAMP
 class ReiTestCase(unittest.TestCase):
     def setUp(self):
         self._scraper = Rei(save_data_path=DATA_PATH)
+        self._bike_type = 'specialty'
+        self._categories = ['mountain', 'road', 'specialty',
+                            'hybrid', 'electric']
 
     def test_get_categories(self):
-        categories = ['mountain', 'road', 'kids',
-                      'specialty', 'hybrid', 'electric']
-
         result = self._scraper._get_categories()
         print('\nCategories:', result)
-        self.assertEqual(len(categories), len(result),
-                         msg=f'Expected {len(categories)}; result {len(result)}')
+        self.assertEqual(len(self._categories), len(result),
+                         msg=f'Expected {len(self._categories)}; result {len(result)}')
         for key in result.keys():
-            self.assertTrue(key in categories,
-                            msg=f'{key} is not in {categories}!')
+            self.assertTrue(key in self._categories,
+                            msg=f'{key} is not in {self._categories}!')
+
+    def test_get_subtypes(self):
+        bike_type = 'specialty'
+        expected_subtypes = ['cruiser', 'folding', 'cargo']
+        result = self._scraper._get_subtypes()
+        print('Subtypes:', result)
+        self.assertEqual(len(self._categories), len(result))
+        for subtype in result[bike_type].keys():
+            self.assertTrue(subtype in expected_subtypes,
+                            msg=f'{subtype} not in {expected_subtypes}')
 
     def test_get_prods_listings(self):
-        bike_type = 'road'
-        categories = self._scraper._get_categories()
-        bike = categories[bike_type]['href'].split('/')[-1]
-        data = json.loads(
-            self._scraper._fetch_prod_listing_view(
-                page_size=self._scraper._page_size, bike=bike
+        categories = self._scraper._get_subtypes()
+        for subtype, values in categories[self._bike_type].items():
+            # only need subtype url name not entire endpoint
+            bike = values['href'].split('/')[-1]
+            data = json.loads(
+                self._scraper._fetch_prod_listing_view(
+                    page_size=30, bike=bike
+                )
             )
-        )
 
+            self._scraper._get_prods_on_current_listings_page(
+                data, self._bike_type, subtype
+            )
         # Verify product listings fetch
-        self._scraper._get_prods_on_current_listings_page(data, bike_type)
         num_prods = len(self._scraper._products)
-        self.assertTrue(num_prods > 5,
+        self.assertTrue(num_prods > 2,
                         msg=f'There are {num_prods} product first page.')
         self._scraper._write_prod_listings_to_csv()
 
     def test_parse_specs(self):
-        bike_type = 'road'
         prods_csv_path = os.path.join(DATA_PATH, TIMESTAMP,
                                       'rei_prods_all.csv')
         # Verify parsing product specs
         specs = self._scraper.get_product_specs(get_prods_from=prods_csv_path,
-                                                bike_type=bike_type,
+                                                bike_type=self._bike_type,
                                                 to_csv=False)
         num_prods = len(self._scraper._products)
         num_specs = len(specs)
         self.assertEqual(num_prods, num_specs,
                          msg=f'Products size: {num_prods}, Specs size: {num_specs}')
         self._scraper._write_prod_specs_to_csv(specs=specs,
-                                               bike_type=bike_type)
+                                               bike_type=self._bike_type)
 
         # Verify spec fieldnames has minimum general fields:
         expected = ['site', 'product_id', 'frame',
